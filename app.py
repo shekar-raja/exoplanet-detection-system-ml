db1eddaa849effd34fdf006c552490a98b79d4e1
Raja Shekar <rajashekarb.dev@gmail.com>
2024-11-07 10:43:06 +0000

    test app

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+import torch
+
+model = torch.load("models/mlp_exoplanet_classifier.pth")
+
+st.write("""
+# My first app
+Hello *world!*
+""")
+
+df = pd.read_csv("dataset/exoTest.csv")
+st.line_chart(df)
